986047251fd5282d5c644be86cd113c7ddea0a9e
Hashim Maniyar <PES2202101204@pesu.pes.edu>
2025-03-13 03:36:18 +0530

    commiting_after_changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -87,7 +87,7 @@ function App() {
               </Button>
 
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>Shopsphere</Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
